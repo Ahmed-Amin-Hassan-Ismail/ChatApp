6a037b3495471cc9a5110bf2603761c75c5df0d2
Ahmed Amin <AhmedAminHassanIsmail@gmail.com>
2022-11-23 20:27:57 +0200

    add lottie package manager

---
+++ ChatApp/ChatApp/View/EmptyMessagesView.swift
@@ -0,0 +1,20 @@
+//
+//  EmptyMessagesView.swift
+//  ChatApp
+//
+//  Created by Ahmed Amin on 23/11/2022.
+//
+
+import SwiftUI
+
+struct EmptyMessagesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EmptyMessagesView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmptyMessagesView()
+    }
+}
